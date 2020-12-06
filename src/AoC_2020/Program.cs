8cf401ba9504242340ce9714fe4459db1e31d925
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2020-12-06 16:30:29 +0100

    Bump AoCHelper and replace invocation to deprecated method.

--- src/AoC_2020/Program.cs
+++ src/AoC_2020/Program.cs
@@ -5,7 +5,7 @@ System.Console.WriteLine(args.Length);
 
 if (args.Length == 0)       // dotnet run
 {
-    Solver.SolveLast(clearConsole: false);
+    Solver.SolveLast(new SolverConfiguration { ClearConsole = false });
 }
 else if (args.Length == 1 && args[0].Contains("all", System.StringComparison.CurrentCultureIgnoreCase))     // dotnet run all | dotnet run --all
 {
@@ -15,5 +15,5 @@ else        // dotnet run 1 2 5 10
 {
     var indexes = args.Select(arg => uint.TryParse(arg, out var index) ? index : uint.MaxValue);
 
-    Solver.Solve(indexes.Where(i => i < uint.MaxValue));
+    Solver.Solve(indexes.Where(i => i < uint.MaxValue), new SolverConfiguration { ShowOverallResults = false, ClearConsole = false });
 }
