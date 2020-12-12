4437e0eeb742ca1bf41846f38a11093181accaf9
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2020-12-12 13:54:19 +0100

    [Day 12] Simplify part 2.

--- src/AoC_2020/Day_12.cs
+++ src/AoC_2020/Day_12.cs
@@ -12,8 +12,9 @@ namespace AoC_2020
     {
         private readonly List<Instruction> _instructions;
 
-        private static readonly Dictionary<ShipAction, Func<Instruction, ShipState, ShipState>> _movements = new()
+        private static readonly Dictionary<ShipAction, Func<Instruction, ShipState, ShipState>> Movements = new()
         {
+#pragma warning disable S1121 // Assignments should not be made from within sub-expressions - Records with pattern
             [ShipAction.Down] = (ins, state) => (state with { Position = state.Position.Move(Direction.Down, ins.Value) }),
             [ShipAction.Up] = (ins, state) => (state with { Position = state.Position.Move(Direction.Up, ins.Value) }),
             [ShipAction.Left] = (ins, state) => (state with { Position = state.Position.Move(Direction.Left, ins.Value) }),
@@ -22,42 +23,40 @@ namespace AoC_2020
             [ShipAction.TurnRight] = (_, state) => (state with { Direction = state.Direction.TurnRight() }),
             [ShipAction.Turn180] = (_, state) => (state with { Direction = state.Direction.Turn180() }),
             [ShipAction.KeepMovin] = (ins, state) => (state with { Position = state.Position.Move(state.Direction, ins.Value) })
+#pragma warning restore S1121 // Assignments should not be made from within sub-expressions
         };
 
-        private static readonly Dictionary<ShipAction, Func<Instruction, ShipState, Point, (ShipState, Point)>> _movementsPart2 = new()
+        private static readonly Dictionary<ShipAction, Func<Instruction, Point, Point, (Point, Point)>> MovementsPart2 = new()
         {
             [ShipAction.Down] = (ins, state, waypoint) => (state, waypoint.Move(Direction.Down, ins.Value)),
             [ShipAction.Up] = (ins, state, waypoint) => (state, waypoint.Move(Direction.Up, ins.Value)),
             [ShipAction.Left] = (ins, state, waypoint) => (state, waypoint.Move(Direction.Left, ins.Value)),
             [ShipAction.Right] = (ins, state, waypoint) => (state, waypoint.Move(Direction.Right, ins.Value)),
 
-            [ShipAction.TurnLeft] = (_, state, waypoint) => (state, Rotate(state.Position, waypoint, 0.5 * Math.PI)),
-            [ShipAction.TurnRight] = (_, state, waypoint) => (state, Rotate(state.Position, waypoint, -0.5 * Math.PI)),
-            [ShipAction.Turn180] = (_, state, waypoint) => (state, Rotate(state.Position, waypoint, Math.PI)),
+            [ShipAction.TurnLeft] = (_, state, waypoint) => (state, Rotate(state, waypoint, 0.5 * Math.PI)),
+            [ShipAction.TurnRight] = (_, state, waypoint) => (state, Rotate(state, waypoint, -0.5 * Math.PI)),
+            [ShipAction.Turn180] = (_, state, waypoint) => (state, Rotate(state, waypoint, Math.PI)),
             [ShipAction.KeepMovin] = (ins, state, waypoint) => (
-                state with
-                {
-                    Position = new Point(
-                    state.Position.X + (ins.Value * (waypoint.X - state.Position.X)),
-                    state.Position.Y + (ins.Value * (waypoint.Y - state.Position.Y)))
-                },
                 new Point(
-                    waypoint.X + (ins.Value * (waypoint.X - state.Position.X)),
-                    waypoint.Y + (ins.Value * (waypoint.Y - state.Position.Y))))
+                    state.X + (ins.Value * (waypoint.X - state.X)),
+                    state.Y + (ins.Value * (waypoint.Y - state.Y))),
+                new Point(
+                    waypoint.X + (ins.Value * (waypoint.X - state.X)),
+                    waypoint.Y + (ins.Value * (waypoint.Y - state.Y))))
         };
 
-        public static Point Rotate(Point permanentPoint, Point pointToBeRotated, double angle)
+        public static Point Rotate(Point origin, Point point, double angle)
         {
             var sinAngle = Math.Sin(angle);
             var cosAngle = Math.Cos(angle);
 
             return new Point(
-                x: (cosAngle * (pointToBeRotated.X - permanentPoint.X))
-                    - (sinAngle * (pointToBeRotated.Y - permanentPoint.Y))
-                    + permanentPoint.X,
-                y: (sinAngle * (pointToBeRotated.X - permanentPoint.X))
-                    + (cosAngle * (pointToBeRotated.Y - permanentPoint.Y))
-                    + permanentPoint.Y);
+                x: (cosAngle * (point.X - origin.X))
+                    - (sinAngle * (point.Y - origin.Y))
+                    + origin.X,
+                y: (sinAngle * (point.X - origin.X))
+                    + (cosAngle * (point.Y - origin.Y))
+                    + origin.Y);
         }
 
         public Day_12()
@@ -72,7 +71,7 @@ namespace AoC_2020
 
             foreach (var instruction in _instructions)
             {
-                shipState = _movements[instruction.Action](instruction, shipState);
+                shipState = Movements[instruction.Action](instruction, shipState);
             }
 
             return initialState.Position.ManhattanDistance(shipState.Position).ToString();
@@ -80,18 +79,18 @@ namespace AoC_2020
 
         public override string Solve_2()
         {
-            var initialState = new ShipState(new Point(0, 0), Direction.Right);
+            var initialShipPosition = new Point(0, 0);
             var initialWaypoint = new Point(10, 1);
 
-            var shipState = initialState;
-            var wayPoint = initialWaypoint;
+            var shipPosition = initialShipPosition;
+            var waypoint = initialWaypoint;
 
             foreach (var instruction in _instructions)
             {
-                (shipState, wayPoint) = _movementsPart2[instruction.Action](instruction, shipState, wayPoint);
+                (shipPosition, waypoint) = MovementsPart2[instruction.Action](instruction, shipPosition, waypoint);
             }
 
-            return initialState.Position.ManhattanDistance(shipState.Position).ToString();
+            return initialShipPosition.ManhattanDistance(shipPosition).ToString();
         }
 
         private IEnumerable<Instruction> ParseInput()
