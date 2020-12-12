447151107b422a857b8764886441b5232b9e8800
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2020-12-12 16:35:12 +0100

    [Day 12] Reduce memory allocation by using a tuple rather than ShipState record. Make enum and instruction record private.

--- src/AoC_2020/Day_12.cs
+++ src/AoC_2020/Day_12.cs
@@ -12,37 +12,38 @@ namespace AoC_2020
     {
         private readonly List<Instruction> _instructions;
 
-        private static readonly Dictionary<ShipAction, Func<Instruction, ShipState, ShipState>> Movements = new()
+        private static readonly Dictionary<ShipAction, Func<Instruction, (Point Position, Direction Direction), (Point, Direction)>> Movements = new()
         {
-#pragma warning disable S1121 // Assignments should not be made from within sub-expressions - Records with pattern
-            [ShipAction.Down] = (ins, state) => (state with { Position = state.Position.Move(Direction.Down, ins.Value) }),
-            [ShipAction.Up] = (ins, state) => (state with { Position = state.Position.Move(Direction.Up, ins.Value) }),
-            [ShipAction.Left] = (ins, state) => (state with { Position = state.Position.Move(Direction.Left, ins.Value) }),
-            [ShipAction.Right] = (ins, state) => (state with { Position = state.Position.Move(Direction.Right, ins.Value) }),
-            [ShipAction.TurnLeft] = (_, state) => (state with { Direction = state.Direction.TurnLeft() }),
-            [ShipAction.TurnRight] = (_, state) => (state with { Direction = state.Direction.TurnRight() }),
-            [ShipAction.Turn180] = (_, state) => (state with { Direction = state.Direction.Turn180() }),
-            [ShipAction.KeepMovin] = (ins, state) => (state with { Position = state.Position.Move(state.Direction, ins.Value) })
-#pragma warning restore S1121 // Assignments should not be made from within sub-expressions
+            [ShipAction.Down] = (ins, state) => (state.Position.Move(Direction.Down, ins.Value), state.Direction),
+            [ShipAction.Up] = (ins, state) => (state.Position.Move(Direction.Up, ins.Value), state.Direction),
+            [ShipAction.Left] = (ins, state) => (state.Position.Move(Direction.Left, ins.Value), state.Direction),
+            [ShipAction.Right] = (ins, state) => (state.Position.Move(Direction.Right, ins.Value), state.Direction),
+
+            [ShipAction.TurnLeft] = (_, state) => (state.Position, state.Direction.TurnLeft()),
+            [ShipAction.TurnRight] = (_, state) => (state.Position, state.Direction.TurnRight()),
+            [ShipAction.Turn180] = (_, state) => (state.Position, state.Direction.Turn180()),
+
+            [ShipAction.KeepMovin] = (ins, state) => (state.Position.Move(state.Direction, ins.Value), state.Direction)
         };
 
-        private static readonly Dictionary<ShipAction, Func<Instruction, Point, Point, (Point, Point)>> MovementsPart2 = new()
+        private static readonly Dictionary<ShipAction, Func<Instruction, Point, Point, (Point ship, Point waypoint)>> MovementsPart2 = new()
         {
-            [ShipAction.Down] = (ins, state, waypoint) => (state, waypoint.Move(Direction.Down, ins.Value)),
-            [ShipAction.Up] = (ins, state, waypoint) => (state, waypoint.Move(Direction.Up, ins.Value)),
-            [ShipAction.Left] = (ins, state, waypoint) => (state, waypoint.Move(Direction.Left, ins.Value)),
-            [ShipAction.Right] = (ins, state, waypoint) => (state, waypoint.Move(Direction.Right, ins.Value)),
+            [ShipAction.Down] = (ins, ship, waypoint) => (ship, waypoint.Move(Direction.Down, ins.Value)),
+            [ShipAction.Up] = (ins, ship, waypoint) => (ship, waypoint.Move(Direction.Up, ins.Value)),
+            [ShipAction.Left] = (ins, ship, waypoint) => (ship, waypoint.Move(Direction.Left, ins.Value)),
+            [ShipAction.Right] = (ins, ship, waypoint) => (ship, waypoint.Move(Direction.Right, ins.Value)),
 
-            [ShipAction.TurnLeft] = (_, state, waypoint) => (state, waypoint.RotateCounterclockwise(state, 90, isRadians: false)),
-            [ShipAction.TurnRight] = (_, state, waypoint) => (state, waypoint.RotateClockwise(state, 90, isRadians: false)),
-            [ShipAction.Turn180] = (_, state, waypoint) => (state, waypoint.RotateClockwise(state, 180, isRadians: false)),
-            [ShipAction.KeepMovin] = (ins, state, waypoint) => (
+            [ShipAction.TurnLeft] = (_, ship, waypoint) => (ship, waypoint.RotateCounterclockwise(ship, 90, isRadians: false)),
+            [ShipAction.TurnRight] = (_, ship, waypoint) => (ship, waypoint.RotateClockwise(ship, 90, isRadians: false)),
+            [ShipAction.Turn180] = (_, ship, waypoint) => (ship, waypoint.RotateClockwise(ship, 180, isRadians: false)),
+
+            [ShipAction.KeepMovin] = (ins, ship, waypoint) => (
                 new Point(
-                    state.X + (ins.Value * (waypoint.X - state.X)),
-                    state.Y + (ins.Value * (waypoint.Y - state.Y))),
+                    ship.X + (ins.Value * (waypoint.X - ship.X)),
+                    ship.Y + (ins.Value * (waypoint.Y - ship.Y))),
                 new Point(
-                    waypoint.X + (ins.Value * (waypoint.X - state.X)),
-                    waypoint.Y + (ins.Value * (waypoint.Y - state.Y))))
+                    waypoint.X + (ins.Value * (waypoint.X - ship.X)),
+                    waypoint.Y + (ins.Value * (waypoint.Y - ship.Y))))
         };
 
         public Day_12()
@@ -52,7 +53,7 @@ namespace AoC_2020
 
         public override string Solve_1()
         {
-            var initialState = new ShipState(new Point(0, 0), Direction.Right);
+            var initialState = (Position: new Point(0, 0), Direction: Direction.Right);
             var shipState = initialState;
 
             foreach (var instruction in _instructions)
@@ -113,14 +114,12 @@ namespace AoC_2020
                 yield return new Instruction(action, value);
             }
         }
+
+        private enum ShipAction
+        {
+            TurnLeft, TurnRight, Turn180, Up, Down, Right, Left, KeepMovin
+        }
+
+        private record Instruction(ShipAction Action, int Value);
     }
-
-    public enum ShipAction
-    {
-        TurnLeft, TurnRight, Turn180, Up, Down, Right, Left, KeepMovin
-    }
-
-    public record Instruction(ShipAction Action, int Value);
-
-    public record ShipState(Point Position, Direction Direction);
 }
