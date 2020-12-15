687e7fafac3d2ccbe3e60f437a32a7188946a814
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2020-12-15 23:34:25 +0100

    [Day 15] Add solutions using List and array, both of them cutting Dictionary time by more than half.

---
+++ src/AoC_2020.Benchmarks/Day_15_Benchmark.cs
@@ -0,0 +1,18 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace AoC_2020.Benchmarks
+{
+    public class Day_15_Benchmark : BaseDayBenchmark
+    {
+        private readonly Day_15 _problem = new();
+
+        [Benchmark]
+        public string PlayMemoryGame_Dictionary() => _problem.PlayMemoryGame_Dictionary(30_000_000);
+
+        [Benchmark]
+        public string PlayMemoryGame_List() => _problem.PlayMemoryGame_List(30_000_000);
+
+        [Benchmark]
+        public string PlayMemoryGame_Array() => _problem.PlayMemoryGame_Array(30_000_000);
+    }
+}
--- src/AoC_2020/Day_15.cs
+++ src/AoC_2020/Day_15.cs
@@ -14,11 +14,16 @@ namespace AoC_2020
             _input = File.ReadAllText(InputFilePath).Split(',').Select(int.Parse).ToList();
         }
 
-        public override string Solve_1() => PlayMemoryGame(2020);
+        public override string Solve_1() => PlayMemoryGame_Array(2020);
 
-        public override string Solve_2() => PlayMemoryGame(30_000_000);
+        public override string Solve_2() => PlayMemoryGame_Array(30_000_000);
 
-        private string PlayMemoryGame(int targetTurn)
+        /// <summary>
+        /// ~2s
+        /// </summary>
+        /// <param name="targetTurn"></param>
+        /// <returns></returns>
+        internal string PlayMemoryGame_Dictionary(int targetTurn)
         {
             var history = new Dictionary<int, int>(targetTurn);
             for (int index = 0; index < _input.Count - 1; ++index)
@@ -35,6 +40,64 @@ namespace AoC_2020
                     turn - 1,
                     history.TryGetValue(previousTurnNumber, out var oldTurn)
                         ? turn - 1 - oldTurn
+                        : 0);
+            }
+
+            return previousTurnNumber.ToString();
+        }
+
+        /// <summary>
+        /// ~850ms.
+        /// Using each element's index as number and its value as saved turn.
+        /// </summary>
+        /// <param name="targetTurn"></param>
+        /// <returns></returns>
+        internal string PlayMemoryGame_List(int targetTurn)
+        {
+            var history = new List<int>(new int[targetTurn + 1]);
+            for (int index = 0; index < _input.Count - 1; ++index)
+            {
+                history[_input[index]] = index + 1;
+            }
+
+            var turn = _input.Count;
+            var previousTurnNumber = _input.Last();
+
+            while (++turn <= targetTurn)
+            {
+                (history[previousTurnNumber], previousTurnNumber) = (
+                    turn - 1,
+                    history[previousTurnNumber] != 0
+                        ? turn - 1 - history[previousTurnNumber]
+                        : 0);
+            }
+
+            return previousTurnNumber.ToString();
+        }
+
+        /// <summary>
+        /// ~750ms.
+        /// Using each element's index as number and its value as saved turn
+        /// </summary>
+        /// <param name="targetTurn"></param>
+        /// <returns></returns>
+        internal string PlayMemoryGame_Array(int targetTurn)
+        {
+            var history = new int[targetTurn + 1];
+            for (int index = 0; index < _input.Count - 1; ++index)
+            {
+                history[_input[index]] = index + 1;
+            }
+
+            var turn = _input.Count;
+            var previousTurnNumber = _input.Last();
+
+            while (++turn <= targetTurn)
+            {
+                (history[previousTurnNumber], previousTurnNumber) = (
+                    turn - 1,
+                    history[previousTurnNumber] != 0
+                        ? turn - 1 - history[previousTurnNumber]
                         : 0);
             }
 
