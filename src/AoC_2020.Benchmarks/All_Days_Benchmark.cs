b0b533701b92cb02cec136514b7f2f30e396d54f
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2020-12-16 01:30:48 +0100

    Add All_Days_Benchmark and run them as a separted CI job.

---
+++ src/AoC_2020.Benchmarks/All_Days_Benchmark.cs
@@ -0,0 +1,29 @@
+﻿using AoCHelper;
+using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace AoC_2020.Benchmarks
+{
+    public class All_Days_Benchmark : BaseDayBenchmark
+    {
+        private readonly List<Type> _allDays = Assembly.GetAssembly(typeof(Day_01))
+                .GetTypes()
+                .Where(type => typeof(BaseProblem).IsAssignableFrom(type) && !type.IsInterface && !type.IsAbstract)
+                .ToList();
+
+        [Benchmark]
+        public void SolveAll()
+        {
+            foreach (var type in _allDays)
+            {
+                var instance = Activator.CreateInstance(type) as BaseDay;
+
+                instance.Solve_1();
+                instance.Solve_2();
+            }
+        }
+    }
+}
--- src/AoC_2020.Benchmarks/BaseDayBenchmark.cs
+++ src/AoC_2020.Benchmarks/BaseDayBenchmark.cs
@@ -1,12 +1,12 @@
 ﻿using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Diagnostics.Windows.Configs;
+//using BenchmarkDotNet.Diagnostics.Windows.Configs;
 
 namespace AoC_2020.Benchmarks
 {
     [MarkdownExporterAttribute.GitHub]
     [HtmlExporter]
     [MemoryDiagnoser]
-    [NativeMemoryProfiler]
+    //[NativeMemoryProfiler]
     public class BaseDayBenchmark
     {
     }
