4146e73518488b684c5d0f23866195c310da79b3
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2020-12-23 18:03:38 +0100

    Solve Day 23 part 2.

--- src/AoC_2020/Day_23.cs
+++ src/AoC_2020/Day_23.cs
@@ -2,6 +2,7 @@
 using FileParser;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -10,16 +11,20 @@ namespace AoC_2020
 {
     public class Day_23 : BaseDay
     {
-        private readonly List<int> _input;
+        private readonly LinkedList<int> _input;
 
         public Day_23()
         {
             _input = ParseInput();
         }
 
-        public override string Solve_1()
+        public override string Solve_1() => Part1_LinkedList();
+
+        public override string Solve_2() => Part2_LinkedList();
+
+        private string Part1_List()
         {
-            var result = PlayCrubCups(_input, 3, 100);
+            var result = PlayCrubCups_List(ParseInput().ToList(), 100).ToList();
 
             var firstIndex = result.IndexOf(1);
 
@@ -33,8 +38,125 @@ namespace AoC_2020
             return solution.ToString();
         }
 
-        public static List<int> PlayCrubCups(List<int> initialLabelling, int numberOfCupsToRemove, int turns)
+        private string Part1_LinkedList()
         {
+            var result = PlayCrubCups_LinkedList_Enhanced(ParseInput(), 100).ToList();
+
+            var firstIndex = result.IndexOf(1);
+
+            var solution = new StringBuilder();
+            var count = firstIndex + result.Count;
+            for (int i = firstIndex + 1; i < count; ++i)
+            {
+                solution.Append(result[i % result.Count]);
+            }
+
+            return solution.ToString();
+        }
+
+        private string Part2_List()
+        {
+            for (int i = _input.Max() + 1; i <= 1_000_000; ++i)
+            {
+                _input.AddLast(i);
+            }
+
+            var result = PlayCrubCups_List(_input.ToList(), 10_000_000);
+
+            var firstNode = result.IndexOf(1);
+
+            return ((long)result[firstNode + 1] * result[firstNode + 2])
+                .ToString();
+        }
+
+        private string Part2_LinkedList()
+        {
+            for (int i = _input.Max() + 1; i <= 1_000_000; ++i)
+            {
+                _input.AddLast(i);
+            }
+
+            var result = PlayCrubCups_LinkedList_Enhanced(_input, 10_000_000);
+
+            var firstNode = result.Find(1)!;
+
+            var next = firstNode.Next ?? result.First!;
+            var nextNext = next.Next ?? result.First!;
+
+            return ((long)next.Value * nextNext.Value)
+                .ToString();
+        }
+
+        /// <summary>
+        /// Similar to <see cref="PlayCrubCups_LinkedList(LinkedList{int}, int, int)"/>, but saving each node in a dictionary
+        /// to avoid using <see cref="LinkedList{T}.Find(T)"/>
+        /// </summary>
+        /// <param name="initialLabelling"></param>
+        /// <param name="numberOfCupsToRemove"></param>
+        /// <param name="turns"></param>
+        /// <returns></returns>
+        private static LinkedList<int> PlayCrubCups_LinkedList_Enhanced(LinkedList<int> initialLabelling, int turns)
+        {
+            const int numberOfCupsToRemove = 3;
+            var cups = initialLabelling;
+            LinkedListNode<int> NextNode(LinkedListNode<int> node) => node.Next ?? cups?.First ?? throw new ArgumentNullException(nameof(initialLabelling));
+
+            var nodeDictionary = new Dictionary<int, LinkedListNode<int>>();
+            var currentNode = cups?.First ?? throw new ArgumentNullException(nameof(initialLabelling));
+            for (int i = 0; i < cups.Count; ++i)
+            {
+                nodeDictionary.Add(currentNode!.Value, currentNode);
+                currentNode = currentNode.Next;
+            }
+
+            var minLabel = cups.Min();
+            var maxLabel = cups.Max();
+
+            var current = cups.Last ?? throw new ArgumentNullException(nameof(initialLabelling));
+
+            for (int turn = 1; turn <= turns; ++turn)
+            {
+                current = NextNode(current);
+
+                var cupsToRemove = new List<LinkedListNode<int>>(numberOfCupsToRemove) { NextNode(current) };
+                for (int i = 1; i <= numberOfCupsToRemove - 1; ++i)
+                {
+                    cupsToRemove.Add(NextNode(cupsToRemove.Last()));
+                }
+
+                cupsToRemove.ForEach(c => cups.Remove(c));
+
+                var destinationCupLabel = (current.Value - 1) >= minLabel ? (current.Value - 1) : maxLabel;
+                while (cupsToRemove.Select(n => n.Value).Contains(destinationCupLabel))
+                {
+                    if (--destinationCupLabel < minLabel)
+                    {
+                        destinationCupLabel = maxLabel;
+                    }
+                }
+
+                var destinationCup = nodeDictionary[destinationCupLabel];
+
+                for (int i = 0; i < cupsToRemove.Count; ++i)
+                {
+                    cups.AddAfter(destinationCup, cupsToRemove[i]);
+                    destinationCup = cupsToRemove[i];
+                }
+            }
+
+            return cups;
+        }
+
+        /// <summary>
+        /// Bottleneck in <see cref="List{T}.Remove(T)(int, T)"/> (70%) and <see cref="List{T}.IndexOf(T)"/> (30%)
+        /// </summary>
+        /// <param name="initialLabelling"></param>
+        /// <param name="numberOfCupsToRemove"></param>
+        /// <param name="turns"></param>
+        /// <returns></returns>
+        private static List<int> PlayCrubCups_List(List<int> initialLabelling, int turns)
+        {
+            const int numberOfCupsToRemove = 3;
             var cups = initialLabelling.ConvertAll(_ => _);
 
             var minLabel = cups.Min();
@@ -86,19 +208,62 @@ namespace AoC_2020
             return cups;
         }
 
-        public override string Solve_2()
+        /// <summary>
+        /// Bottleneck in <see cref="LinkedList{T}.Find(T)"/> (100%)
+        /// </summary>
+        /// <param name="initialLabelling"></param>
+        /// <param name="numberOfCupsToRemove"></param>
+        /// <param name="turns"></param>
+        /// <returns></returns>
+        private static LinkedList<int> PlayCrubCups_LinkedList(LinkedList<int> initialLabelling, int turns)
         {
-            var solution = string.Empty;
+            const int numberOfCupsToRemove = 3;
+            var cups = initialLabelling;
+            LinkedListNode<int> NextNode(LinkedListNode<int> node) => node.Next ?? cups?.First ?? throw new ArgumentNullException(nameof(initialLabelling));
 
-            return solution;
+            var minLabel = cups.Min();
+            var maxLabel = cups.Max();
+
+            var current = cups.Last ?? throw new ArgumentNullException(nameof(initialLabelling));
+
+            for (int turn = 1; turn <= turns; ++turn)
+            {
+                current = NextNode(current);
+
+                var cupsToRemove = new List<LinkedListNode<int>>(numberOfCupsToRemove) { NextNode(current) };
+                for (int i = 1; i <= numberOfCupsToRemove - 1; ++i)
+                {
+                    cupsToRemove.Add(NextNode(cupsToRemove.Last()));
+                }
+
+                cupsToRemove.ForEach(c => cups.Remove(c));
+
+                var destinationCupLabel = (current.Value - 1) >= minLabel ? (current.Value - 1) : maxLabel;
+                while (cupsToRemove.Select(n => n.Value).Contains(destinationCupLabel))
+                {
+                    if (--destinationCupLabel < minLabel)
+                    {
+                        destinationCupLabel = maxLabel;
+                    }
+                }
+
+                var destinationCup = cups.Find(destinationCupLabel)!;
+
+                for (int i = 0; i < cupsToRemove.Count; ++i)
+                {
+                    cups.AddAfter(destinationCup, cupsToRemove[i]);
+                    destinationCup = cupsToRemove[i];
+                }
+            }
+
+            return cups;
         }
 
-        private List<int> ParseInput()
+        private LinkedList<int> ParseInput()
         {
-            return File.ReadAllText(InputFilePath)
+            return new LinkedList<int>(File.ReadAllText(InputFilePath)
                 .Trim()
-                .Select(ch => int.Parse(ch.ToString()))
-                .ToList();
+                .Select(ch => int.Parse(ch.ToString())));
         }
     }
 }
--- tests/AoC_2020.Test/SolutionTests.cs
+++ tests/AoC_2020.Test/SolutionTests.cs
@@ -31,7 +31,7 @@ namespace AoC_2020.Test
             //[InlineData(typeof(Day_20), "", "")]
             [InlineData(typeof(Day_21), "2428", "bjq,jznhvh,klplr,dtvhzt,sbzd,tlgjzx,ctmbr,kqms")]
             [InlineData(typeof(Day_22), "31957", "33212")]
-            [InlineData(typeof(Day_23), "47382659", "")]
+            [InlineData(typeof(Day_23), "47382659", "42271866720")]
             public void Test(Type type, string sol1, string sol2)
             {
                 var instance = Activator.CreateInstance(type) as BaseDay;
