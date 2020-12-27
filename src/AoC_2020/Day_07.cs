bd7571e4da2a9563fa0d795b97faddd6cfbe95a8
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2020-12-27 15:43:32 +0100

    Revert "Revert Day 7 to HashSet: FastHashSet doesn't get on as well as it could with reflection."
    This reverts commit 90a609c30a2ec1118f11688f1ddef273282a373d.

--- src/AoC_2020/Day_07.cs
+++ src/AoC_2020/Day_07.cs
@@ -23,9 +23,9 @@ namespace AoC_2020
         {
             return BagsThatCanContainABag(MyBagName).Count.ToString();
 
-            HashSet<Bag> BagsThatCanContainABag(string bagId, HashSet<Bag>? result = null)
+            FastHashSet<Bag> BagsThatCanContainABag(string bagId, FastHashSet<Bag>? result = null)
             {
-                result ??= new HashSet<Bag>();
+                result ??= new FastHashSet<Bag>();
 
                 foreach (var bag in _input.Values.Where(node => node.Children.Any(ch => ch.Key.Id == bagId)))
                 {
